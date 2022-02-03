5dff5a30dd88eecb1827c42f47376be404234e3f
Kamesh <kameshkumar4602@gmail.com>
2022-02-03 21:38:50 +0530

    Heading of HomePage

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Container, Box, Text } from "@chakra-ui/react";
 
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans" color="black">
+          Chit-Chat App
+        </Text>
+      </Box>
+      <Box></Box>
+    </Container>
+  );
 };
 
 export default HomePage;

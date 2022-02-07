438c01aa3859fea0d892d09a8ae143d1a7c9fb62
Kamesh <kameshkumar4602@gmail.com>
2022-02-07 16:48:57 +0530

    authorization middleware in searching

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -68,7 +68,7 @@ const allUsers = asyncHandler(async(req, res) => {
 
         ]
     } : {}
-    const users = await (await User.find(keyword))
+    const users = await User.find(keyword).find({ _id: { $ne: req.user._id } })
     res.send(users)
 
 })
